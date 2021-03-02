866aa3653e2c86396bf6608da102c953d24590f7
KeslieBox <kesliebox@gmail.com>
2021-03-01 18:13:23 -0600

    commit tree 0aae59039f9e708bcde1c9d2d7fd9989846dfa66

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,61 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    if !params[:new_landmark][:name].empty? || !params[:new_title][:name].empty? 
+      landmark = @figure.landmarks.create(params[:new_landmark])
+      title = @figure.titles.create(params[:new_title])
+    end
+
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by(params[:id])
+    @figure_name = @figure.name
+    @figure_id = @figure.id
+    @titles = Title.all
+    @title = Title.find_by(params[:id])
+    @landmark = Landmark.find_by(params[:id])
+    @landmark_name = @landmark.name
+    @figures = Figure.all
+
+    erb :"/figures/edit"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by(params[:id])
+    @titles = Title.all
+    erb :"/figures/show"
+  end
+
+  patch '/figures/:id' do
+
+    @figure = Figure.find_by(id: params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:title][:name].empty?
+      @figure.titles.create(params[:title]) 
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks.create(params[:landmark])
+    end
+
+    redirect "/figures/#{@figure.id}"
+  end
+
 end
